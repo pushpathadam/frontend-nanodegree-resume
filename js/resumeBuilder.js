6a99c7a103a0e31aaa207367c0ca812306c58d1b
pushpathadam <tom@verycleverdesign.com>
2015-02-27 13:31:18 -0800

    comment out everything but json

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -6,38 +6,107 @@ var bio = {
     "email": "tom@verycleverdesign.com",
     "github": "pushpathadam",
     "twitter": "@pushpathadam",
-    "location": "verycleverdesign.com",
+    "location": "verycleverdesign.com"
   },
   "welcomeMessage": "Web Designer-Analyst, Fx-Artist, Dabbler in Architecture & Furniture making, Father, Husband. I read fast, I learn fast, I get sh#t done.",
   "skills": [
       "html5","css3", "google analytics", "python"
       ],
-  "biopic": "images/fry.jpg",
-//  "display": "function taking no parameters",
+  "biopic": "images/fry.jpg"
 };
 
-var formattedName = HTMLheaderName.replace("%data%", bio.name);
-var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+//var formattedName = HTMLheaderName.replace("%data%", bio.name);
+//var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+//var formattedGeneric = '<li class="flex-item"><span class="orange-text">' + 'crap' + '</span><span class="white-text">' + 'crap'+ '</span></li>';
+//var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+//var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+//var formattedTwitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
+//var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
+//var formattedLocation =  HTMLlocation.replace("%data%", bio.contacts.location);
+//var formattedBiopic =  HTMLbioPic.replace("%data%", bio.biopic);var formattedWelcome = HTMLWelcomeMsg.replace("%data%", bio.welcomeMessage);
 
-var formattedGeneric = '<li class="flex-item"><span class="orange-text">' + 'crap' + '</span><span class="white-text">' + 'crap'+ '</span></li>';
-var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
-var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
-var formattedTwitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
-var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
-var formattedLocation =  HTMLlocation.replace("%data%", bio.contacts.location);
+var work = {
+  "jobs": [
+    {
+      "employer": "Acme",
+      "title": "Boss",
+      "dates": "not sure",
+      "location": "Los Angeles, CA",
+      "description": "something"
+    },
+    {
+      "employer": "Acme2.0",
+      "title": "chief2",
+      "dates": "not sure",
+      "location": "Montreal, Canada",
+      "description": "something"
+    }
+  ]
+}
+var projects = {
+  "project":[
+    {
+    "title": "something",
+    "dates": "some days",
+    "description": "something i did",
+    "image": "images/fry.jpg"
+    },
+    {
+    "title": "something2",
+    "dates": "some days",
+    "description": "something i did",
+    "image": "images/fry.jpg"
+    }
+  ]
+};
 
-var formattedBiopic =  HTMLbioPic.replace("%data%", bio.biopic);var formattedWelcome = HTMLWelcomeMsg.replace("%data%", bio.welcomeMessage);
+var education = {
+  "schools":[
+    {
+      "name": "Texas A&M",
+      "city": "College Station, TX",
+      "degree": "MS Visualization Sciences",
+      "majors": ["Visualization"],
+      "dates": "1996",
+      "url": "http://example.com"
+    },
+    {
+      "name": "Mc Gill",
+      "city": "Montreal Canada",
+      "degree": "B Architecture",
+      "majors": ["Architecture"],
+      "dates": "1992",
+      "url": "http://example.com"
+    }
+  ],
+  "onlineCourses":[
+    {
+      "classes": "something",
+      "title": "certificate in ",
+      "school": "institute of",
+      "dates": "Feb 2014",
+      "url": "www.something.com"
+    },
+    {
+      "classes": "something2",
+      "title": "certificate in ",
+      "school": "institute of",
+      "dates": "July 2014",
+      "url": "www.something.com"
+    }
+  ]
+};
 
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
+//$("#header").prepend(formattedRole);
+//$("#header").prepend(formattedName);
 
-$("#header").append(formattedBiopic);
-$("#header").append(formattedWelcome);
-$("#topContacts").prepend(formattedLocation);
-$("#topContacts").prepend(formattedGithub);
-$("#topContacts").prepend(formattedTwitter);
-$("#topContacts").prepend(formattedEmail);
-$("#topContacts").prepend(formattedMobile);
+//$("#header").append(formattedBiopic);
+//$("#header").append(formattedWelcome);
+//$("#topContacts").prepend(formattedLocation);
+//$("#topContacts").prepend(formattedGithub);
+//$("#topContacts").prepend(formattedTwitter);
+//$("#topContacts").prepend(formattedEmail);
+//$("#topContacts").prepend(formattedMobile);
 //$("#topContacts").prepend(formattedGeneric);
 
-console.log(formattedTwitter)
+//console.log(formattedTwitter)
