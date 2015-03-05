1dec95ec042dd6d069ad87bfb619bf9fbcd02fa1
pushpathadam <tom@verycleverdesign.com>
2015-03-05 12:41:11 -0800

    Map Marker Works

--- js/helper.js
+++ js/helper.js
@@ -140,7 +140,7 @@ function initializeMap() {
     for (var job in work.jobs) {
       locations.push(work.jobs[job].location);
     }
-    console.log(locations);
+    //console.log(locations);
     return locations;
   }
 
@@ -171,27 +171,20 @@ function initializeMap() {
     // which is simply a string that contains the overlay's HTML.
     //Right now it's set to name, which is just the name of the city.
     //Try learning some HTML and CSS to make the overlay really stand out!
+    console.log(name)
+    console.log(locationDescriptions.textfield(name));
 
     var contentString = '<div id="content">'+
       '<div id="siteNotice">' +
       '</div>'+
       '<h1 id="firstHeading" class="firstHeading">' + name + '</h1>'+
       '<div id="bodyContent">'+
-      '<p><b>' + name + '</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-      'sandstone rock formation in the southern part of the '+
-      'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-      'south west of the nearest large town, Alice Springs; 450&#160;km '+
-      '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-      'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-      'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-      'Aboriginal people of the area. It has many springs, waterholes, '+
-      'rock caves and ancient paintings. Uluru is listed as a World '+
-      'Heritage Site.</p>'+
-      '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-      'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-      '(last visited June 22, 2009).</p>'+
+      '<p><b>' + name + '</b>,'+ locationDescriptions.textfield(name)[0] + '</p>'+
+      '<p>Attribution: '+ name + ', '+
+      '<a href="'+ locationDescriptions.textfield(name)[1] + '">'+ locationDescriptions.textfield(name)[1] +'</a> '+
       '</div>'+
       '</div>';
+
     var infoWindow = new google.maps.InfoWindow({
       content: contentString
     });
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -12,139 +12,132 @@ var bio = {
   "skills": [
       "breathing","eating", "drinking", "you get the picture"
       ],
-  "biopic": "images/fry.jpg"
+  "biopic": "images/fry.jpg",
+  "displayBio": function () {
+    var formattedName = HTMLheaderName.replace("%data%", bio.name);
+    $("#header").prepend(formattedName);
+
+    var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+    $("#header").prepend(formattedRole);
+
+    var formattedBiopic =  HTMLbioPic.replace("%data%", bio.biopic);
+    $("#header").append(formattedBiopic);
+
+
+    var formattedWelcome = HTMLWelcomeMsg.replace("%data%", bio.welcomeMessage);
+    $("#header").append(formattedWelcome);
+
+    var formattedLocation =  HTMLlocation.replace("%data%", bio.contacts.location);
+    $("#topContacts").prepend(formattedLocation);
+
+    var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
+    $("#topContacts").prepend(formattedGithub);
+
+    var formattedTwitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
+    $("#topContacts").prepend(formattedTwitter);
+
+    var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+    $("#topContacts").prepend(formattedEmail);
+
+    var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+    $("#topContacts").prepend(formattedMobile);
+
+    // var formattedGeneric = '<li class="flex-item"><span class="orange-text">' + 'crap' + '</span><span class="white-text">' + 'crap'+ '</span></li>';
+    //$("#topContacts").prepend(formattedGeneric);
+  }
 };
 
-var formattedName = HTMLheaderName.replace("%data%", bio.name);
-var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
-var formattedGeneric = '<li class="flex-item"><span class="orange-text">' + 'crap' + '</span><span class="white-text">' + 'crap'+ '</span></li>';
-var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
-var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
-var formattedTwitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
-var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
-var formattedLocation =  HTMLlocation.replace("%data%", bio.contacts.location);
-var formattedBiopic =  HTMLbioPic.replace("%data%", bio.biopic);var formattedWelcome = HTMLWelcomeMsg.replace("%data%", bio.welcomeMessage);
-
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
-
-$("#header").append(formattedBiopic);
-$("#header").append(formattedWelcome);
-$("#topContacts").prepend(formattedLocation);
-$("#topContacts").prepend(formattedGithub);
-$("#topContacts").prepend(formattedTwitter);
-$("#topContacts").prepend(formattedEmail);
-$("#topContacts").prepend(formattedMobile);
-$("#topContacts").prepend(formattedGeneric);
+bio.displayBio();
 
 var work = {
   "jobs": [
     {
       "employer": "Acme4.0",
       "title": "Boss",
-      "dates": "July 2013",
+      "dates": "2000-13",
       "location": "Los Angeles, CA",
       "description": "bon-vivant"
     },
     {
       "employer": "Acme3.0",
       "title": "Minion",
-      "dates": "August 1992",
-      "location": "Burlingame, CA",
+      "dates": "1996-2000",
+      "location": "Palo Alto, CA",
       "description": "Eat, drink, repeat cycle"
     },
-    {
-      "employer": "Acme2.0",
-      "title": "Minion",
-      "dates": "August 1992",
-      "location": "Agra, India",
-      "description": "Eat, drink, repeat cycle"
-    },
-    {
-      "employer": "Acme1.0",
-      "title": "Minion",
-      "dates": "August 1992",
-      "location": "Ottawa, Canada",
-      "description": "Eat, drink, repeat cycle"
+  ],
+  "displayWork" : function(){
+    for (job in work.jobs){
+      $("#workExperience").append(HTMLworkStart);
+      var formattedEmployer = HTMLworkEmployer.replace("%data%",work.jobs[job].employer);
+      var formattedTitle = HTMLworkTitle.replace("%data%",work.jobs[job].title);
+      var formattedEmployerTitle = formattedEmployer + formattedTitle;
+      $(".work-entry:last").append(formattedEmployerTitle);
+
+      var formattedDates = HTMLworkDates.replace("%data%",work.jobs[job].dates);
+      $(".work-entry:last").append(formattedDates);
+
+      var formattedLocation = HTMLworkLocation.replace("%data%",work.jobs[job].location);
+      $(".work-entry:last").append(formattedLocation);
+
+      var formattedDescription = HTMLworkDescription.replace("%data%",work.jobs[job].description);
+      $(".work-entry:last").append(formattedDescription);
     }
-  ]
-}
-
-function displayWork() {
-  for (job in work.jobs){
-    $("#workExperience").append(HTMLworkStart);
-    var formattedEmployer = HTMLworkEmployer.replace("%data%",work.jobs[job].employer);
-    var formattedTitle = HTMLworkTitle.replace("%data%",work.jobs[job].title);
-    var formattedEmployerTitle = formattedEmployer + formattedTitle;
-    $(".work-entry:last").append(formattedEmployerTitle);
-
-    var formattedDates = HTMLworkDates.replace("%data%",work.jobs[job].dates);
-    $(".work-entry:last").append(formattedDates);
-
-    var formattedLocation = HTMLworkLocation.replace("%data%",work.jobs[job].location);
-    $(".work-entry:last").append(formattedLocation);
-
-    var formattedDescription = HTMLworkDescription.replace("%data%",work.jobs[job].description);
-    $(".work-entry:last").append(formattedDescription);
-
-    //console.log(formattedEmployer);
   }
 }
-displayWork();
+
+work.displayWork();
 
 function locationizer(work_obj){
     var workLocations = [];
     for (job in work_obj.jobs){
-      console.log(work_obj.jobs[job].location);
+      //console.log(work_obj.jobs[job].location);
       workLocations.push(work_obj.jobs[job].location);
     }
     return workLocations;
 };
 
-console.log(locationizer(work));
-
-
-
+//console.log(locationizer(work));
 
 var projects = {
   "projects":[
     {
     "title": "something",
-    "dates": "2016",
+    "dates": "2017",
     "description": "something i did",
     "images": ["images/fry.jpg","images/fry.jpg"]
     },
     {
     "title": "something2",
-    "dates": "2017",
+    "dates": "2016",
     "description": "something i did later",
     "images": ["images/fry.jpg","images/fry.jpg"]
     }
-  ]
+  ],
+  "displayProjects": function () {
+    for (project in projects.projects){
+      $("#projects").append(HTMLprojectStart);
+
+      var formattedProjectTitle = HTMLprojectTitle.replace("%data%",projects.projects[project].title);
+      $(".project-entry:last").append(formattedProjectTitle);
+
+      var formattedProjectDates = HTMLprojectDates.replace("%data%",projects.projects[project].dates);
+      $(".project-entry:last").append(formattedProjectDates);
+
+      var formattedProjectDescription = HTMLprojectDescription.replace("%data%",projects.projects[project].description);
+      $(".project-entry:last").append(formattedProjectDescription);
+
+      if (projects.projects[project].images.length > 0) {
+        for (image in projects.projects[project].images) {
+          var formattedProjectImage = HTMLprojectImage.replace("%data%",projects.projects[project].images[image]);
+          $(".project-entry:last").append(formattedProjectImage);
+        }
+      };
+    }
+  }
 };
 
-projects.display = function () {
-  for (project in projects.projects){
-    $("#projects").append(HTMLprojectStart);
-
-    var formattedProjectTitle = HTMLprojectTitle.replace("%data%",projects.projects[project].title);
-    $(".project-entry:last").append(formattedProjectTitle);
-
-    var formattedProjectDates = HTMLprojectDates.replace("%data%",projects.projects[project].dates);
-    $(".project-entry:last").append(formattedProjectDates);
-
-    var formattedProjectDescription = HTMLprojectDescription.replace("%data%",projects.projects[project].description);
-    $(".project-entry:last").append(formattedProjectDescription);
-
-    if (projects.projects[project].images.length > 0) {
-      for (image in projects.projects[project].images) {
-        var formattedProjectImage = HTMLprojectImage.replace("%data%",projects.projects[project].images[image]);
-        $(".project-entry:last").append(formattedProjectImage);
-      }
-    };
-  }
-}
-projects.display();
+projects.displayProjects();
 
 var education = {
   "schools":[
@@ -167,71 +160,113 @@ var education = {
     {
       "title": "certificate in ",
       "school": "institute of",
-      "dates": "Feb 2014",
+      "dates": "2014",
       "url": "www.something.com"
     },
     {
       "title": "certificate in ",
       "school": "institute of",
-      "dates": "July 2014",
+      "dates": "2014",
       "url": "www.something.com"
     }
-  ]
+  ],
+  "displayEducation": function () {
+    for (school in education.schools){
+      $("#education").append(HTMLschoolStart);
+
+      var formattedSchoolName = HTMLschoolName.replace("%data%",education.schools[school].name);
+      $(".education-entry:last").append(formattedSchoolName);
+
+      var formattedSchoolLocation = HTMLschoolLocation.replace("%data%",education.schools[school].location);
+      $(".education-entry:last").append(formattedSchoolLocation);
+
+      var formattedSchoolDegree = HTMLschoolDegree.replace("%data%",education.schools[school].degree);
+      $(".education-entry:last").append(formattedSchoolDegree);
+
+      if (education.schools[school].majors.length > 0) {
+        for (major in education.schools[school].majors) {
+          var formattedSchoolMajor = HTMLschoolMajor.replace("%data%",education.schools[school].majors[major]);
+          $(".project-entry:last").append(formattedSchoolMajor);
+        }
+      };
+
+      var formattedSchoolDates = HTMLschoolDates.replace("%data%",education.schools[school].dates);
+      $(".education-entry:last").append(formattedSchoolDates);
+
+      if (projects.projects[project].images.length > 0) {
+        for (image in projects.projects[project].images) {
+          var formattedProjectImage = HTMLprojectImage.replace("%data%",projects.projects[project].images[image]);
+          $(".project-entry:last").append(formattedProjectImage);
+        }
+      };
+    };
+
+    $(".education-entry:last").append(HTMLonlineClasses);
+
+    for (course in education.onlineCourses){
+
+      var formattedOnlineTitle = HTMLonlineTitle.replace("%data%",education.onlineCourses[course].title);
+      $(".education-entry:last").append(formattedOnlineTitle);
+
+      var formattedOnlineSchool = HTMLonlineSchool.replace("%data%",education.onlineCourses[course].school);
+      $(".education-entry:last").append(formattedOnlineSchool);
+
+      var formattedOnlineDates = HTMLonlineDates.replace("%data%",education.onlineCourses[course].dates);
+      $(".education-entry:last").append(formattedOnlineDates);
+
+      var formattedOnlineURL = HTMLonlineURL.replace("%data%",education.onlineCourses[course].url);
+      $(".education-entry:last").append(formattedOnlineURL);
+    }
+  }
 };
 
+education.displayEducation();
 
-education.display = function () {
-  for (school in education.schools){
-    $("#education").append(HTMLschoolStart);
+var locationDescriptions = {
+  "places" : [
+    {
+      "location":"Encino",
+      "description": "Encino is an affluent neighborhood in the San Fernando Valley region of Los Angeles, California. Encino has three public and eight private schools, including two private high schools, and the community has been the home of many notable people. -Wikipedia",
+      "attribution": "http://en.wikipedia.org/wiki/Encino,_Los_Angeles"
 
-    var formattedSchoolName = HTMLschoolName.replace("%data%",education.schools[school].name);
-    $(".education-entry:last").append(formattedSchoolName);
+    },
+    {
+      "location": "Los Angeles, CA",
+      "description": "Los Angeles, officially the City of Los Angeles, often known by its initials L.A., is a major city in California's Southern California region, approximately 342 miles south of San Francisco. Wikipedia",
+      "attribution": "http://en.wikipedia.org/wiki/Los_Angeles"
 
-    var formattedSchoolLocation = HTMLschoolLocation.replace("%data%",education.schools[school].location);
-    $(".education-entry:last").append(formattedSchoolLocation);
+    },
+    {
+      "location": "Palo Alto, CA",
+      "description": "Palo Alto is a charter city located in the northwest corner of Santa Clara County, California, in the San Francisco Bay Area of the United States.",
+      "attribution": "http://en.wikipedia.org/wiki/Palo_Alto,_California"
 
-    var formattedSchoolDegree = HTMLschoolDegree.replace("%data%",education.schools[school].degree);
-    $(".education-entry:last").append(formattedSchoolDegree);
-
-    if (education.schools[school].majors.length > 0) {
-      for (major in education.schools[school].majors) {
-        var formattedSchoolMajor = HTMLschoolMajor.replace("%data%",education.schools[school].majors[major]);
-        $(".project-entry:last").append(formattedSchoolMajor);
+    },
+    {
+      "location":"College Station, TX",
+      "description": "College Station is a city in Brazos County, Texas, situated in East Central Texas in the heart of the Brazos Valley. It is north from both Houston and Austin.",
+      "attribution": "http://en.wikipedia.org/wiki/College_Station,_Texas"
+    },
+    {
+      "location": "Montreal, QC, Canada",
+      "description": "Montreal is a city in the Canadian province of Quebec. It is the largest city in the province, the second-largest in Canada and the 9th-largest in North America.",
+      "attribution": "http://en.wikipedia.org/wiki/Montreal"
+    }
+  ],
+  "textfield": function(place){
+    for (var x in locationDescriptions.places) {
+      // sloppy check that proper city name matches partial
+      var tmpPlace = place.split(",")[0];
+      var tmpCurrentPlace = (locationDescriptions.places[x].location).split(",")[0];
+      if (tmpPlace === tmpCurrentPlace) {
+        locationText = [ locationDescriptions.places[x].description, locationDescriptions.places[x].attribution ];
+        return locationText;
       }
     };
-
-    var formattedSchoolDates = HTMLschoolDates.replace("%data%",education.schools[school].dates);
-    $(".education-entry:last").append(formattedSchoolDates);
-
-    if (projects.projects[project].images.length > 0) {
-      for (image in projects.projects[project].images) {
-        var formattedProjectImage = HTMLprojectImage.replace("%data%",projects.projects[project].images[image]);
-        $(".project-entry:last").append(formattedProjectImage);
-      }
-    };
-  };
-
-  $(".education-entry:last").append(HTMLonlineClasses);
-
-  for (course in education.onlineCourses){
-
-    var formattedOnlineTitle = HTMLonlineTitle.replace("%data%",education.onlineCourses[course].title);
-    $(".education-entry:last").append(formattedOnlineTitle);
-
-    var formattedOnlineSchool = HTMLonlineSchool.replace("%data%",education.onlineCourses[course].school);
-    $(".education-entry:last").append(formattedOnlineSchool);
-
-    var formattedOnlineDates = HTMLonlineDates.replace("%data%",education.onlineCourses[course].dates);
-    $(".education-entry:last").append(formattedOnlineDates);
-
-    var formattedOnlineURL = HTMLonlineURL.replace("%data%",education.onlineCourses[course].url);
-    $(".education-entry:last").append(formattedOnlineURL);
-
   }
 }
 
-education.display();
-
+//console.log(locationDescriptions.textfield("Encino"));
 
 function inName(name){
     name = name.trim().split(" ");
@@ -247,5 +282,3 @@ function inName(name){
 $("#header").prepend(internationalizeButton);
 
 $("#mapDiv").append(googleMap);
-
-//console.log(formattedTwitter)
